0da019302d8b9b078df5d0d95deed83c504bd19e
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-08-18 20:23:44 +0600

    tailwind css installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='w-full h-screen flex justify-center items-center'>
+        <h1 className='font-bold text-5xl text-indigo-600'>Using Tailwind CSS First Time</h1>
+      </div>
     </div>
   );
 }
